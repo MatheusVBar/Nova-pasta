76d9306602fda555d002683d6bb558129c5b60c4
Matheus Vinicius <matheusvbar@gmail.com>
2026-03-04 23:43:04 +0000

    numpy

---
+++ numPy2.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pylot as plt
+
+print (f"OpenCV versão: {cv2.__version__}")
+print (f"NumPy versão: {np.__version__}")
+
+# tesseract OCR (OCR offlne) - instalar separado:
+# macOS: brew install tesseract
+# ubuntu: sudo apt install tesseract-ocr
+#windows: https://github.com/UB-Mannheim/tesseract/wiki
